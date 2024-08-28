ca4372105b81b5c95acb7a424e8909e5e14c7f4a
Kat W-E <kat@fonix.com>
2024-08-28 19:41:57 +0100

    creates a reset total press-ups button and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update, :update_press_ups]
 
   def show
-    @user = current_user
+    @challenge_to_date = calculate_total_press_ups_to_date
   end
 
   def edit
-    @user = current_user
   end
 
   def update
-    @user = current_user
     if @user.update(user_params)
       redirect_to user_path, notice: 'Profile updated successfully.'
     else
@@ -18,7 +17,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def update_press_ups
+    @user.total_press_ups = calculate_total_press_ups_to_date
+    if @user.save
+      redirect_to user_path, notice: "Your total press-ups have been updated to the current date."
+    else
+      redirect_to user_path, alert: "There was a problem updating your total press-ups."
+    end
+  end
+
   private
+
+  def calculate_total_press_ups_to_date
+    # Calculate the total press-ups the user should have done from their start date to today
+    days_since_start = (Date.today - @user.start_date).to_i + 1
+    (days_since_start * (days_since_start + 1)) / 2
+  end
+
+  def set_user
+    @user = current_user
+  end
 
   def user_params
     params.require(:user).permit(:username, :start_date, :current_day, :total_press_ups, :press_ups_done_today)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resource :user, only: [:show, :edit, :update] do
+    patch :update_press_ups, on: :collection
+  end
   resources :press_ups, only: [:index]
   resources :logs, only: [:create, :index]
   delete 'reset_logs', to: 'logs#reset_logs', as: 'reset_logs'
