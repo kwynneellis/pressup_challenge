376359b69a8acd1f5a666f2a54f8b3602d37c4af
Kat W-E <kat@fonix.com>
2024-08-28 18:56:52 +0100

    adds press-up amount to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_press_ups_today
+
+  private
+
+  def set_press_ups_today
+    if user_signed_in?
+      @press_ups_today = calculate_press_ups_for_today
+    end
+  end
+
+  def calculate_press_ups_for_today
+    # Ensure current_user is defined
+    if current_user
+      # Calculate the number of press-ups required today
+      days_since_start = (Date.today - current_user.start_date).to_i + 1
+      days_since_start
+    else
+      0
+    end
+  end
 
   protected
 
