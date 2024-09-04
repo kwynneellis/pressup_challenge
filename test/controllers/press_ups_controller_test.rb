0d1b3b89500889c5e9212a7d5cf8ff89502125af
Kat W-E <kat@fonix.com>
2024-09-04 21:06:01 +0100

    removes pressUpsController, test and assoiciated views. Comments out controller tests.

--- test/controllers/press_ups_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PressUpsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get press_ups_index_url
-    assert_response :success
-  end
-end
