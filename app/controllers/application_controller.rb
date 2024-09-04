0b6418affe741c4dd39caeacffe9cca325d6aec5
Kat W-E <kat@fonix.com>
2024-09-04 20:07:04 +0100

    creates navbar_footer and updates user show page and original navbar. gets rid of the dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include ActionView::Helpers::NumberHelper
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_info
+  before_action :set_info, if: :user_signed_in?
 
   private
 
