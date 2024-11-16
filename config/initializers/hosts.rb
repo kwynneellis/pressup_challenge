872a6f427efad51d0cb554410d22a0a067e0e5ef
Kat W-E <kat@fonix.com>
2024-11-16 14:00:30 +0000

    add heroku to hosts.rb

--- config/initializers/hosts.rb
+++ config/initializers/hosts.rb
@@ -1,1 +1,8 @@
-Rails.application.config.hosts << "challenges.wynneellis.com"
+Rails.application.configure do
+  # Other configuration...
+
+  # Allow Heroku host dynamically
+  config.hosts << /herokuapp\.com\z/
+  # Allow challenges.wynneellis.com
+  config.hosts << /challenges.wynneellis.com\z/
+end
