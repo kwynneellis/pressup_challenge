63a28837416a3bf5160cf53a94e068bbc1b69020
Jon Gilbraith <jon@hobocode.io>
2024-12-24 16:26:07 +0000

    Bodge host validation

--- config/initializers/hosts.rb
+++ config/initializers/hosts.rb
@@ -1,9 +1,1 @@
-Rails.application.configure do
-
-  # Allow Heroku host dynamically
-  config.hosts << /herokuapp\.com\z/
-  config.hosts << "challenge-365-c00837f2297c.herokuapp.com"
-
-  # Allow challenges.wynneellis.com
-  config.hosts << "challenges.wynneellis.com"
-end
+Rails.application.config.hosts = nil
